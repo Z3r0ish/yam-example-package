14b95490ca8282ed9e5851955154851f26093b5e
Z3r0ish <Z3r0ish@github.com>
2020-09-24 18:27:25 -0400

    an example package

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # yam-example-package
+
+This is just an example package that does nothing, may make it do something later down the line
---
+++ install.sh
@@ -0,0 +1,6 @@
+source ./info.yam
+
+echo "This is your config file"
+echo $CONFIG
+echo "This is your config file's output"
+cat $CONFIG
